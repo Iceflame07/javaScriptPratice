1e93e6381f6d8eba2a1ed469bd1cbc154a89698d
Iceflame07 <Chibizz007@gmail.com>
2024-11-13 06:02:05 +0100

    <trackexpenses>

--- trackexpenses.js
+++ trackexpenses.js
@@ -1,6 +1,7 @@
-let trackexpenses = {groceries: 150, dinningout: 100, transportation: 50, entertainment: 80};
+let expenses = {groceries: 150, dinningout: 100, transportation: 50, entertainment: 80};
 let sum = 0;
 
-for (const sum in trackexpenses) {
-  console.log(`trackexpenses.${sum} = ${trackexpenses[sum]}`);
+for (const sum in expenses) {
+  console.log(`expenses.${sum} = ${expenses[sum]}`);
 };
+  module.exports = {expenses};
---
+++ trackexpenses.test.js
@@ -0,0 +1,8 @@
+const {expenses} = require ("./trackexpenses")
+
+    test("sum in expenses",()=>{
+        let expenses = {groceries: 150, dinningout: 100, transportation: 50, entertainment: 80};
+        let result = expenses;
+        let sum = [380];
+        expect(result).toEqual(expenses);
+    })
