69f3f337aba283c1b440df337555a558708b0c1c
Iceflame07 <Chibizz007@gmail.com>
2024-11-13 05:43:38 +0100

    <scoreIncrease>

--- scoreIncrease.test.js
+++ scoreIncrease.test.js
@@ -1,7 +1,7 @@
 const {upgrade} = require => ("./scoreIncrease")
     test("add five to each numbers", ()=>{
         const scoreIncrease = [85, 92, 78, 88, 95];
-        let result = upgrade(scoreIncrease);
+        let result = upgrade;
         let answer = [90, 97, 83, 93, 100];
-        expect(result).toEqual(answer);
+        expect(result).toEqual(upgrade);
     });
